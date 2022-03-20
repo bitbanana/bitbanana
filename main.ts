7a73b8e8bcd4c658f51dc96a7c2ad0eae4701a8b
Rubydog <76613417+rbdog@users.noreply.github.com>
2022-03-20 15:21:19 +0900

    リファクタ

--- main.ts
+++ main.ts
@@ -49,8 +49,12 @@ type PoSBlock<T> = {
 
 type Block = PoSBlock<Transaction>;
 
+// 抽選に参加しているバリデーター
+// ブロックチェーンは運営がいないので、その代わりの人たち
 type Validator = {
   address: string;
+  // 応募金 敷金みたいな感じで帰ってくるので安心
+  // FIXME: これの数 * バリデーター数 でループすることになるので上限をつけた方が良さそう
   token: number;
 };
 
@@ -70,41 +74,59 @@ class FullNode {
   // 検証ように流れてきた候補のブロックたち
   // バリデーターと紐付けておいた方が良さそう
   candidateBlocks: Block[] = [];
-  // 抽選に参加しているバリデーターたち と 応募金(token と呼ばれる)
-  // ブロックチェーンは運営がいないので、その代わりの人たち
-  // 応募金というか敷金みたいな感じで帰ってくるので安心
-  // Map 使いたくないので、うまいこと名前を変えるかクラスを作りたい
-  validators = new Map<string, number>();
+  // 抽選に参加しているバリデーターたち
+  validators: Validator[] = [];
 
   async loadLocalBlockchain() {
-    const repository = new BlockRepository();
-    this.blockchain = await repository.loadLocalBlockChain();
+    const r = new BlockRepository();
+    this.blockchain = await r.loadLocalBlockchain();
   }
 
-  // FIXME: - 差分だけ書き込めた方が良い
-  async saveLocalBlockChain() {
-    const repository = new BlockRepository();
-    await repository.saveLocalBlockchain(this.blockchain);
+  async saveLocalBlockchain() {
+    const r = new BlockRepository();
+    await r.saveLocalBlockchain(this.blockchain);
   }
 
   // 他のフルノードと同期する
-  async syncWithOtherNodes() {
+  validatorIsOk(v: Validator): boolean {
     // FIXME: 実装
+    return true;
   }
 
   async startServer() {
-    // ストレージから読み込み -> 他のノードと同期
+    // ストレージから読み込み
     await this.loadLocalBlockchain();
-    await this.syncWithOtherNodes();
 
+    // ここでWS接続
     // ポートを開いてサーバー起動
     const tcpPort = 5001;
-    // FIXME: - 実装
+    // FIXME: 実装
 
-    // バリデーターを選ぶことができるサーバーで実行
-    // FIXME: 30秒に一回くらい
-    pickValidator();
-    handleConn("WSコネクション");
+    // 以下、接続後...
+    // 接続されたバリデーターたち
+    const validators: Validator[] = [];
+    for (const v of validators) {
+      // 不正チェック
+      if (this.validatorIsOk(v)) {
+        // validators に追加する
+        // FIXME: 候補ブロックもバリデーター型の中に入れてしまおうかな
+        // お互いが持っているブロックチェーンを交換し合う
+      } else {
+        // トークンを没収
+      }
+    }
+
+    // FIXME: 前の抽選から30秒に一回くらいの頻度で実施 誰が実行命令するんやろ? FullNodeよりも偉いサーバー?
+    const pickedV = pickValidator(this.validators);
+
+    // 当選したバリデーターが提案したブロックをブロックチェーンに追加する
+    // 先に提案してもらうか、当選してから提案してもらうかは自由
+    // バリデーターへの手数料をお忘れなく
+    // FIXME: 実装
+
+    // 他のバリデーターにも最新のブロックチェーンを配布
+    // 受け取った側はローカルへ保存
+    // FIXME: 実装
   }
 }
 
@@ -114,49 +136,27 @@ class FullNodeServer {
 
 // 抽選プールを作って、ブロックを追加する権利があるwinnerを決める
 // ステーク(預けておいた)されたトークンの量に応じて当選率が上がる
-function pickValidator() {
+function pickValidator(validators: Validator[]): string {
   // 抽選の応募者がいない場合は終了
-  if (fullNode.validators.keys.length < 0) {
-    return;
+  if (validators.length < 0) {
+    console.log("バリデーターが不足しています");
+    return "";
   }
 
   // 抽選箱
   const lotteryPool: string[] = [];
   // 全ての抽選者に対して
-  for (let [address, token] of fullNode.validators) {
+  for (let v of validators) {
     // 応募金の数だけ抽選箱にアドレスを追加
-    const range = Array.from(Array(token).keys()); // 0..length
+    const range = Array.from(Array(v.token).keys()); // 0..length
     for (const _ of range) {
-      lotteryPool.push(address);
+      lotteryPool.push(v.address);
     }
   }
 
   // 抽選箱からランダムに一つ取り出す
   const randomIndex = Math.floor(Math.random() * lotteryPool.length);
-  const pickedValidatorAddress = lotteryPool[randomIndex];
-
-  // 当選したバリデーターが提案したブロックをブロックチェーンに追加する
-  // 先に提案してもらうか、当選してから提案してもらうかは自由
-  // バリデーターへの手数料をお忘れなく
-  // FIXME: 実装
-
-  // 他のバリデーターにも最新のブロックチェーンを配布
-  // 受け取ったらローカルへ保存
-  // FIXME: 実装
-}
-
-// FIXME: コネクションはstringではない
-// 接続された時の処理
-function handleConn(conn: string) {
-  // バリデーターが接続されたので validators に追加する
-  // FIXME: 実装
-
-  // 不正なブロックを提案されたら、そのブロックを削除 トークンを没収
-  // そうでない場合は 候補ブロック配列へ追加
-  // FIXME: 実装
-
-  // お互いが持っているブロックチェーンを交換し合う
-  // FIXME: 実装
+  return lotteryPool[randomIndex];
 }
 
 // ブロックが有効かどうか確認する
@@ -178,50 +178,48 @@ function blockIsValid(b: Block, prevB: Block): boolean {
 }
 
 // 新しいブロックを生成
-function generateBlock(
-  oldBlock: Block,
-  data: Transaction,
-  validatorAddress: string,
+function createBlock(
+  prevB: Block,
+  tx: Transaction,
+  validator: string,
 ): Block {
   const time = new Date().toISOString();
-  // ハッシュは仮の値
+  // 仮のハッシュ値でブロックを生成
   const block: Block = {
-    index: oldBlock.index,
+    index: prevB.index + 1,
     time: time,
-    data: data,
-    hash: "仮の値",
-    prev_hash: oldBlock.hash,
-    validator: validatorAddress,
+    data: tx,
+    hash: "temp",
+    prev_hash: prevB.hash,
+    validator: validator,
   };
-  // ハッシュを計算
-  const hash = calcBlockHash(block);
-  block.hash = hash;
+  // 正しいハッシュを計算
+  block.hash = calcBlockHash(block);
   return block;
 }
 
 class BlockRepository {
   // データベースの代わりにするjsonファイル
   filePath = "all_blocks.json";
-  async loadLocalBlockChain(): Promise<Block[]> {
+  async loadLocalBlockchain(): Promise<Block[]> {
     // ファイルのテキストを読み込む
     const text = await Deno.readTextFile(this.filePath);
     // Block型の配列へ変換する
     const allBlocks: Block[] = JSON.parse(text);
     return allBlocks;
   }
-  saveLocalBlockchain(allBlocks: Block[]) {
+  async saveLocalBlockchain(allBlocks: Block[]) {
     // Block型の配列をテキストへ変換する
     const text = JSON.stringify(allBlocks);
     // create: ファイルが存在しない場合は作成 = false
     // append: ファイルが存在する場合でも、上書きせずに末尾に追加 = false
     // 今回は 存在する場合のみ 上書き
     const opt = { append: false, create: false };
-    Deno.writeTextFile(this.filePath, text, opt);
+    await Deno.writeTextFile(this.filePath, text, opt);
   }
 }
 
 // ブロックのハッシュを計算する
-// FIXME: - 一時的にハッシュが仮の状態になってるのどうにかならんか
 function calcBlockHash(b: Block): string {
   // 順番は大切なので仕様を決めておく
   // とりあえずハッシュ以外の全部の情報を詰め込んでおけばOK
