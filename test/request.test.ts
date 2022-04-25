0db1ba4a497a6f65bd9abb0ed72361d5321f4a35
Rubydog <76613417+rbdog@users.noreply.github.com>
2022-04-25 19:24:53 +0900

    roadmap

--- test/request.test.ts
+++ test/request.test.ts
@@ -1,10 +1,25 @@
-export async function testSendReq() {
+export async function sendGetReq() {
   const res = await fetch("http://localhost:8000/test", {
     method: "GET",
   });
   const text = await res.text();
 }
 
+export async function sendPostReq() {
+  const res = await fetch("http://localhost:8000/post", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ nickname: "taro" }),
+  });
+  const text = await res.text();
+}
+
 Deno.test("GETリクエスト送信", async () => {
-  await testSendReq();
+  await sendGetReq();
+});
+
+Deno.test("POSTリクエスト送信", async () => {
+  await sendPostReq();
 });
