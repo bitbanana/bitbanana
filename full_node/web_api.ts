1a371f96c7fbbee0e103f5478a44388031360785
Rubydog <76613417+rbdog@users.noreply.github.com>
2022-04-25 15:12:44 +0900

    async

--- full_node/web_api.ts
+++ full_node/web_api.ts
@@ -1,11 +1,13 @@
 import { Tx } from "../blockchain/mod.ts";
-import { bitFruit, whiteTxList } from "./full_node.ts";
+import { bitFruit, FullNode, whiteTxList } from "./full_node.ts";
+// utils
+import { createSigningKeyPair } from "../utils/signing_key_pair.ts";
 
 // 初回限定ボーナスをもらう
-export async function startBonus(addr: string) {
+export async function startBonus(addr: string): Promise<void> {
   await bitFruit.wallet.initialize();
   const tx = await bitFruit.createStartBonusTx(addr);
-  addWhiteTx(tx);
+  await addWhiteTx(tx);
 }
 
 // 残高照会
@@ -14,6 +16,9 @@ export function balanceInquiry() {
 
 // 処理待ちの Tx を追加する
 // (WebSocket クライアント > サーバー)
-export function addWhiteTx(tx: Tx) {
+export async function addWhiteTx(tx: Tx): Promise<void> {
   whiteTxList.push(tx);
+  const fullNode = new FullNode();
+  const dammyKey = await (await createSigningKeyPair()).publicKey;
+  await fullNode.onReceiveWhiteTx(tx, dammyKey);
 }
