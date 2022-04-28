64d6dfdbf0f58e0a744424ac2f1c750f93957a3a
Rubydog <76613417+rbdog@users.noreply.github.com>
2022-04-28 21:37:39 +0900

    cron

--- bit_fruit/server.ts
+++ bit_fruit/server.ts
@@ -1,4 +1,5 @@
 import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
+import { Cron } from "https://cdn.jsdelivr.net/gh/hexagon/croner@4/src/croner.js";
 
 const addr = "localhost:8000";
 const baseUrl = `http://${addr}`;
@@ -12,3 +13,10 @@ serve((req: Request) => {
     return new Response('{ "result": "NG 存在しないURL" }');
   }
 }, { addr });
+
+// 毎日 0,6,12,18時に実行
+const _ = new Cron("0 0 0,6,12,18 * * *", {
+  timezone: "Asia/Tokyo",
+}, () => {
+  console.log("価格を更新します");
+});
