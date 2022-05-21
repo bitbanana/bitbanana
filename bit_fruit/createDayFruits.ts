728e8879e8d67bad60daed38f4def53867ed638e
Rubydog <76613417+rbdog@users.noreply.github.com>
2022-05-21 17:24:49 +0900

    0.0.5

--- bit_fruit/createDayFruits.ts
+++ bit_fruit/createDayFruits.ts
@@ -1,8 +1,6 @@
 import { DayFruit } from "./types/DayFruit.ts";
-import { DayFruitRepo } from "./DayFruitRepo.ts";
 import { yyyyMMdd } from "../utils/date_format.ts";
 import { Collection } from "../mongo/Collection.ts";
-import { fixedFruits } from "./fixedFruits.ts";
 import { dayFruitConfigs } from "./dayFruitConfigs.ts";
 import { defaultDayFruits } from "./defaultDayFruits.ts";
 
@@ -32,21 +30,28 @@ export async function createDayFruits() {
   // const ytdFruits = await dfRepo.loadFruitsByDate(ytdYyyymmdd);
   let ytdFruits = await c.find({ "yyyymmdd": ytdYyyymmdd });
   console.log(`Step 2...`);
-  if (ytdFruits.length === 0) {
-    console.log(`Yesterday Fruits Not Exist`);
-    ytdFruits = defaultDayFruits(todayYyyymmdd);
-  }
-  for (const ytdF of ytdFruits) {
-    const todayF: DayFruit = {
-      fruit_id: ytdF.fruit_id,
+
+  const defaultFruits = defaultDayFruits(todayYyyymmdd);
+  for (const dfc of dayFruitConfigs) {
+    const defaultFruit = defaultFruits.find((e) => e.fruit_id == dfc.fruit_id);
+    if (defaultFruit == undefined) {
+      throw new Error("default fruit not found");
+    }
+    let ytdFruit = ytdFruits.find((e) => e.fruit_id == dfc.fruit_id);
+    if (ytdFruit == undefined) {
+      ytdFruit = defaultFruit;
+    }
+    const todayFruit: DayFruit = {
+      fruit_id: ytdFruit.fruit_id,
       yyyymmdd: todayYyyymmdd,
       buy_count: 0,
       sell_count: 0,
-      price_ytd: ytdF.price,
-      price: ytdF.price,
+      price_ytd: ytdFruit.price,
+      price: ytdFruit.price,
     };
-    todayFruits.push(todayF);
+    todayFruits.push(todayFruit);
   }
+
   // 保存
   await c.insertMany(todayFruits);
   console.log(`Step Complete`);
